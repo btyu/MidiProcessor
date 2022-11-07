bcf8dd8ad0c8bc36a5bbe176724287f5eaa77acf
btyu <btyu@foxmail.com>
2022-11-07 20:55:01 +0800

    [bug] 0.1.1

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 import setuptools
 
+from midiprocessor.version import __version__
+
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="midiprocessor",  # Replace with your own username
-    version="0.1.0",
+    version=__version__,
     author="Botao Yu",
     author_email="btyu@foxmail.com",
     description="A tool for processing MIDI files.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    # package_data={},
+    package_data={'midiprocessor': ['legacy_tools/tools/midi_model/*', 'key_profile.pickle']},
     entry_points={
         'console_scripts': [
             'mp-batch-encoding = midiprocessor.batch_encoding:main'
